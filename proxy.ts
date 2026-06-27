961e910a096f1e209331fab5a48575d85fb9ce09
Nitin-Campus-ready <nitin@campus-ready.com>
2026-06-27 09:27:43 +0530

    fix:route-issue

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const PUBLIC_ROUTES = ["/login", "/signup"];
+const PUBLIC_ROUTES = ["/login", "/signup", "/"];
 
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
